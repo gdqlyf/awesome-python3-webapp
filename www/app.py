5fd4b9f36c7ab5bf2564ee256818106950353f65
guodong <guodong1250421@gmail.com>
2017-10-09 19:42:52 +0800

    final commit

--- www/app.py
+++ www/app.py
@@ -191,7 +191,7 @@ async def init(loop):
     add_static(app)
     # 启动
     srv = await loop.create_server(app.make_handler(), '0.0.0.0', 9000)
-    logging.info('server started at http://127.0.0.1:9000...')
+    logging.info('server started at http://0.0.0.0:9000...')
     return srv
 
 
